cb492e4b524bdd2981ff047dab984b1dcd1eb16c
Louis Laureys <louis@laureys.me>
2019-05-29 13:04:00 +0200

    Update Phaser to 3.17.0 and use built in scaling

--- src/game.ts
+++ src/game.ts
@@ -5,11 +5,16 @@ import { GameScene } from "./scenes/game-scene";
 import { GameOver } from "./scenes/game-over";
 import { Arcade } from "./utils/arcade"
 
-const config: GameConfig = {
+const config: Phaser.Types.Core.GameConfig = {
     width: 1000,
     height: 625,
+    scale: {
+        mode: Phaser.Scale.ScaleModes.FIT,
+        autoCenter: Phaser.Scale.Center.CENTER_BOTH
+    },
     parent: "game",
     resolution: window.devicePixelRatio, // added
+    // @ts-ignore Issue with Typescript definitions in Phaser 3.17.0
     scene: [BootScene, StartScene, GameScene, GameOver],
     input: {
         keyboard: true
@@ -28,28 +33,12 @@ export class RuimteGruis extends Phaser.Game {
 
     public arcade:Arcade
 
-    constructor(config: GameConfig) {
+    constructor(config: Phaser.Types.Core.GameConfig) {
         super(config)
 
         // create the arcade once, otherwise we keep connecting/disconnecting every scene
         this.arcade = new Arcade()
-
-        window.addEventListener("resize", () => this.resizeGame())
-        this.resizeGame()
     }
-
-    public resizeGame(){
-        // WE HAVE TO SCALE THE CANVAS
-        // PHASER SCALE CONFIG DOCUMENTATION IS INCORRECT 
-        // NOTE: als je zelf scaling uitrekent moet viewport meta-tag weg uit html!!
-        
-        let scale = Math.min(window.innerWidth/1000, window.innerHeight/625)
-        let div : HTMLElement = document.querySelector("#game")! as HTMLElement;
-        let canvas = div.querySelector("canvas")
-        
-        canvas.style.transform = `scale(${scale})`
-        //div.style.transform = `scale(${scale})`
-  }
 }
 
 window.addEventListener("load", () => new RuimteGruis(config))
